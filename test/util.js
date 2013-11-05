bf3f77bfd2f39517c05a7e622da29572de927e78
Matt Calthrop <mcalthrop@gmail.com>
2013-11-05 16:08:45 +0000

    Partial fix for #2: add instructions for setting environment variables in order to run tests

--- test/util.js
+++ test/util.js
@@ -3,6 +3,12 @@
 var contentful = require('..');
 var q = require('q');
 
+if (process.env.CONTENTFUL_ACCESS_TOKEN === undefined) {
+    throw new ReferenceError('Environment variable must be defined: CONTENTFUL_ACCESS_TOKEN');
+}
+if (process.env.CONTENTFUL_MANAGEMENT_HOSTNAME === undefined) {
+    throw new ReferenceError('Environment variable must be defined: CONTENTFUL_MANAGEMENT_HOSTNAME');
+}
 exports.client = contentful.createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   host: process.env.CONTENTFUL_MANAGEMENT_HOSTNAME,
