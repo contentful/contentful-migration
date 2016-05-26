745a23c3d4c4a992e93dde06c74091d7060e076c
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-05-26 11:39:32 +0200

    fix: Fix library name in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'browser-dist'),
     filename: 'contentful-management.js',
-    library: 'contentful-management'
+    library: 'contentfulManagement'
   },
   module: {
     loaders: [
