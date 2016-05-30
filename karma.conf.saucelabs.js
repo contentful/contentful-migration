a94bb0d5ba6f7c37d3176a2d789a0e652f8c4501
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-05-30 17:53:24 +0200

    chore: Update saucelabs browser config

--- karma.conf.saucelabs.js
+++ karma.conf.saucelabs.js
@@ -4,25 +4,26 @@ var browsers = {
   sl_chrome: {
     base: 'SauceLabs',
     browserName: 'chrome',
-    platform: 'Windows 7',
-    version: '48'
+    platform: 'Windows 10',
+    version: '50.0'
   },
   sl_firefox: {
     base: 'SauceLabs',
     browserName: 'firefox',
-    version: '44'
+    platform: 'Windows 10',
+    version: '46.0'
   },
   sl_ie_11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
+    platform: 'Windows 10',
+    version: '11.103'
   },
   sl_edge: {
     base: 'SauceLabs',
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
-    version: '20.10240'
+    version: '13.10586'
   }
 }
 
@@ -31,7 +32,7 @@ module.exports = function (config) {
   karmaBaseConf.reporters.push('saucelabs')
   karmaBaseConf.logLevel = config.LOG_DEBUG
   karmaBaseConf.customLaunchers = browsers
-  karmaBaseConf.captureTimeout = 120000
+  karmaBaseConf.captureTimeout = 200000
   karmaBaseConf.browserDisconnectTolerance = 3
   karmaBaseConf.concurrency = 5
   karmaBaseConf.browsers = Object.keys(browsers)
