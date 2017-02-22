563560d04db7d9674360890c38991d2feb39c5e2
Benedikt Rötsch <benedikt@contentful.com>
2017-02-22 14:30:13 +0100

    refactor(build): adjust babel config to fix the tests

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 var axios = require('axios')
-var contentfulManagement
-try {
-  contentfulManagement = require('./dist/contentful-management').default
-} catch (err) {
-  if (err.code === 'MODULE_NOT_FOUND') {
-    require('babel-register')
-    contentfulManagement = require('./lib/contentful-management').default
-  } else {
-    console.log(err)
-    process.exit(1)
-  }
-}
+var contentfulManagement = require('./dist/contentful-management').default
+
 module.exports = {
   createClient: function (params) {
     return contentfulManagement(axios, params)
