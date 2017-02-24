220b76c09cf5b8cd048fa2d474660a0ec17322d6
Benedikt Rötsch <info@benedikt-roetsch.de>
2017-02-24 10:14:38 +0100

    chore(browser.js): remove old browser entry file

--- browser.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import contentfulManagement from './lib/contentful-management'
-module.exports = {
-  createClient: function (params) {
-    return contentfulManagement(axios, params)
-  }
-}
