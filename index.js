06e0322135d91d40fba32fbb9cd18b4f84799060
Khaled Garbaya <khaled@contentful.com>
2017-03-29 18:16:47 +0200

    fix(build): fix umb build for browser

--- index.js
+++
@@ -1,8 +0,0 @@
-var axios = require('axios')
-var contentfulManagement = require('./dist/contentful-management').default
-
-module.exports = {
-  createClient: function (params) {
-    return contentfulManagement(axios, params)
-  }
-}
--- test/unit/contentful-management-test.js
+++ test/unit/contentful-management-test.js
@@ -1,28 +1,28 @@
 import test from 'tape'
 import sinon from 'sinon'
-import createClient from '../../lib/contentful-management'
-
-const axios = {create: sinon.stub()}
+import {createClient, __RewireAPI__ as createClientRewireApi} from '../../lib/contentful-management'
 
 test('Throws if no accessToken is defined', (t) => {
   t.throws(() => {
-    createClient(axios, {space: 'spaceid'})
+    createClient({space: 'spaceid'})
   }, /Expected parameter accessToken/)
   t.end()
 })
 
 test('Passes along HTTP client parameters', (t) => {
-  createClient.__Rewire__('version', 'version')
-  const createHttpClientStub = sinon.stub()
-  createClient.__Rewire__('createHttpClient', createHttpClientStub)
-  createClient.__Rewire__('createContentfulApi', sinon.stub().returns({}))
+  createClientRewireApi.__Rewire__('version', 'version')
+  createClientRewireApi.__Rewire__('axios', {create: sinon.stub()})
 
-  const client = createClient(axios, {accessToken: 'accesstoken'})
+  const createHttpClientStub = sinon.stub()
+  createClientRewireApi.__Rewire__('createHttpClient', createHttpClientStub)
+  createClientRewireApi.__Rewire__('createContentfulApi', sinon.stub().returns({}))
+
+  const client = createClient({accessToken: 'accesstoken'})
   t.ok(createHttpClientStub.args[0][1].headers['Content-Type'], 'sets the content type')
   t.equals(createHttpClientStub.args[0][1].headers['X-Contentful-User-Agent'], 'contentful-management.js/version', 'sets the user agent header')
   t.ok(client, 'returns a client')
-  createClient.__ResetDependency__('createHttpClient')
-  createClient.__ResetDependency__('wrapHttpClient')
-  createClient.__ResetDependency__('createContentfulApi')
+  createClientRewireApi.__ResetDependency__('createHttpClient')
+  createClientRewireApi.__ResetDependency__('wrapHttpClient')
+  createClientRewireApi.__ResetDependency__('createContentfulApi')
   t.end()
 })
