abf1e9c88b268affaae8fe0f97b75a1932315df3
Stephan Schneider <stephanschndr@gmail.com>
2017-10-23 18:13:38 +0200

    wip: state recorder

--- src/lib/state/change-recorder.ts
+++ src/lib/state/change-recorder.ts
@@ -5,38 +5,132 @@ interface Batch<EntityType> {
   state: State<EntityType>
 }
 
-export default class ChangeRecorder<EntityType> {
-  private committedState: State<EntityType>;
-  private hasOpenBatch: boolean = false;
-  private batches: Batch<EntityType>[] = [];
-  private batchId: string = null;
-  private stagedState: State<EntityType> = null;
+export default class ChangeRecorder<EntityType> implements StateInterface<EntityType> {
+  private committedState: State<EntityType>
+  private stagedState: State<EntityType> = null
+  private hasOpenBatch: boolean = false
+  private batches: Batch<EntityType>[] = []
+  private batchId: string = null
 
-  constructor(initial: Map<String, EntityType> = new Map()) {
-    this.committedState = new State(initial);
+  constructor (initial: Map<String, EntityType> = new Map()) {
+    this.committedState = new State(initial)
   }
 
-  startBatch (id: string) {
-    this.hasOpenBatch = true;
-    this.stagedState = new State();
-    this.batchId = id;
+  async startBatch (id: string) {
+    this.hasOpenBatch = true
+    this.stagedState = new State()
+    this.batchId = id
   }
 
   async endBatch () {
     this.batches.push({
       id: this.batchId,
       state: this.stagedState
-    });
+    })
 
     for await (const [key, value] of this.stagedState.deletionIterator) {
-      await this.committedState.delete(key);
+      await this.committedState.delete(key)
     }
 
     for await (const [key, value] of this.stagedState.entityIterator) {
-      await this.committedState.set(key, value);
+      await this.committedState.set(key, value)
     }
-    this.hasOpenBatch = false;
-    this.stagedState = null;
-    this.batchId = null;
+    this.hasOpenBatch = false
+    this.stagedState = null
+    this.batchId = null
+  }
+
+  async has (key: String) {
+    this.assertOpenBatch()
+
+    const hasStaged = await this.stagedState.has(key)
+    const hasCommited = await this.committedState.has(key)
+
+    if (hasStaged || hasCommited) {
+      return true
+    }
+
+    return false
+  }
+
+  async get (key: String) {
+    this.assertOpenBatch()
+
+    const hasStaged = await this.stagedState.has(key)
+    const hasCommited = await this.committedState.has(key)
+
+    if (!hasStaged && !hasCommited) {
+      throw new Error(`Cannot get key ${key} because it does not exist`)
+    }
+
+    if (hasStaged) {
+      return this.stagedState.get(key)
+    }
+
+    return this.committedState.get(key)
+  }
+
+  async set (key: String, value: EntityType) {
+    this.assertOpenBatch()
+
+    return this.stagedState.set(key, value)
+  }
+
+  async delete (key: String) {
+    this.assertOpenBatch()
+
+    const hasStaged = await this.stagedState.has(key)
+    const hasCommited = await this.committedState.has(key)
+
+    if (!hasStaged && !hasCommited) {
+      throw new Error(`Cannot delete key ${key} because it does not exist`)
+    }
+
+    // Payload needs to be copied to staged state before deleting it
+    if (!hasStaged && hasCommited) {
+      const commitedEntity = await this.committedState.get(key)
+      await this.stagedState.set(key, commitedEntity)
+    }
+
+    return this.stagedState.delete(key)
+  }
+
+  async wasDeleted (key: String) {
+    this.assertOpenBatch()
+
+    const deletedInStaged = await this.stagedState.wasDeleted(key)
+    const deletedInCommited = await this.committedState.wasDeleted(key)
+
+    return deletedInStaged || deletedInCommited
+  }
+
+  async stagedForDeletion (key: String) {
+    this.assertOpenBatch()
+
+    return this.stagedState.wasDeleted(key)
+  }
+
+  async hasCommited (key: String) {
+    return this.committedState.has(key)
+  }
+
+  async getCommited (key: String) {
+    return this.committedState.get(key)
+  }
+
+  async getBatches () {
+    if (this.hasOpenBatch) {
+      throw new Error('Batch needs to be closed before calling getBatches method on it.')
+    }
+    return this.batches
+  }
+
+  // TODO: I wanna be a decorator
+  private assertOpenBatch () {
+    if (this.hasOpenBatch) {
+      return
+    }
+
+    throw new Error('Batch needs to be opened before calling StateInterface methods on it.')
   }
 }
--- src/lib/state/index.ts
+++ src/lib/state/index.ts
@@ -1,90 +1,93 @@
 export interface StateInterface<EntityType> {
-  set(key: String, value: EntityType)
-  get(key: String)
-  has(key: String)
-  delete(key: String)
-  wasDeleted(key: String)
-  getDeletions() : Promise<Map<String, EntityType>>
-  getAll() : Promise<Map<String, EntityType>>
+  set (key: String, value: EntityType)
+  get (key: String)
+  has (key: String)
+  delete (key: String)
+  wasDeleted (key: String)
+  getDeletions? (): Promise<Map<String, EntityType>>
+  getAll? (): Promise<Map<String, EntityType>>
+}
+
+export interface StateIterableInterface<EntityType> {
   entityIterator: AsyncIterable<[String, EntityType]>
   deletionIterator: AsyncIterable<[String, EntityType]>
 }
 
-(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
+(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for('Symbol.asyncIterator')
 
-export class State<EntityType> implements StateInterface<EntityType> {
-  private entities:Map<String, EntityType> = new Map();
-  private deletedEntities: Map<String, EntityType> = new Map();
+export class State<EntityType> implements StateInterface<EntityType>, StateIterableInterface<EntityType> {
+  private entities: Map<String, EntityType> = new Map()
+  private deletedEntities: Map<String, EntityType> = new Map()
 
   constructor (initial: Map<String, EntityType> = new Map()) {
     for (const [key, value] of initial) {
-      this.entities.set(key, value);
+      this.entities.set(key, value)
     }
   }
 
   async set (key: String, value: EntityType) {
-    this.entities.set(key, value);
-    this.deletedEntities.delete(key);
+    this.entities.set(key, value)
+    this.deletedEntities.delete(key)
   }
 
   async get (key: String) {
-    const hasKey = await this.has(key);
+    const hasKey = await this.has(key)
     if (!hasKey) {
-      throw new Error(`Cannot get key ${key} because it does not exist`);
+      throw new Error(`Cannot get key ${key} because it does not exist`)
     }
-    return this.entities.get(key);
+    return this.entities.get(key)
   }
 
   async has (key: String) {
-    return this.entities.has(key);
+    return this.entities.has(key)
   }
 
   async delete (key: String) {
-    const hasKey = await this.has(key);
+    const hasKey = await this.has(key)
 
     if (!hasKey) {
-      throw new Error(`Cannot delete key ${key} because it does not exist`);
+      throw new Error(`Cannot delete key ${key} because it does not exist`)
     }
 
-    const entity = await this.get(key);
-    this.deletedEntities.set(key, entity);
-    this.entities.delete(key);
+    const entity = await this.get(key)
+    this.deletedEntities.set(key, entity)
+    this.entities.delete(key)
   }
 
   async wasDeleted (key: String) {
-    return this.deletedEntities.has(key);
+    return this.deletedEntities.has(key)
   }
 
   async getDeletions () {
-    return this.deletedEntities;
+    return this.deletedEntities
   }
 
   async getAll () {
-    return this.entities;
+    return this.entities
   }
 
-  get entityIterator(): AsyncIterable<[String , EntityType]> {
+  get entityIterator (): AsyncIterable<[String , EntityType]> {
     return {
       [Symbol.asyncIterator]: (): AsyncIterator<[String , EntityType]> =>  {
         const iter = this.entities.entries()
 
         return {
           next: async (): Promise<IteratorResult<[String , EntityType]>> => {
-            return iter.next();
+            return iter.next()
           }
         }
       }
     }
   }
 
-  get deletionIterator(): AsyncIterable<[String , EntityType]> {
+  get deletionIterator (): AsyncIterable<[String , EntityType]> {
     return {
       [Symbol.asyncIterator]: (): AsyncIterator<[String , EntityType]> => {
         const iter = this.deletedEntities.entries()
 
         return {
           next: async (): Promise<IteratorResult<[String , EntityType]>> => {
-            return iter.next();
+            return iter.next()
           }
         }
       }
