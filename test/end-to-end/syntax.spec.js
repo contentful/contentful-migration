884f64ace1cad75e2191548bdf49c9fe9e3a5958
Sergiy Tupchiy <sergiy@contentful.com>
2017-11-09 10:37:42 +0100

    test(e2e): rename misleading tests purpose; added lint ignore config

--- test/end-to-end/syntax.spec.js
+++ test/end-to-end/syntax.spec.js
@@ -5,16 +5,16 @@ const cli = require('./cli');
 
 const SOURCE_TEST_SPACE = process.env.CONTENTFUL_INTEGRATION_SOURCE_SPACE;
 
-describe('invalid payload', function () {
+describe('invalid syntax errors', function () {
   it('outputs the undefined error', function (done) {
     cli()
-      .run(`--space-id ${SOURCE_TEST_SPACE} ./examples/10-payload-undefined.js`)
+      .run(`--space-id ${SOURCE_TEST_SPACE} ./examples/10-syntax-undefined.js`)
       .expect(assert.payload.notDefined('buggybug'))
       .end(done);
   });
 
   it('outputs the missing argument error', function (done) {
-    const script = '11-payload-corrupt.js';
+    const script = '11-syntax-corrupt.js';
     cli()
       .run(`--space-id ${SOURCE_TEST_SPACE} ./examples/${script}`)
       .expect(assert.payload.syntaxError(script, 4, 'missing ) after argument list'))
@@ -22,7 +22,7 @@ describe('invalid payload', function () {
   });
 
   it('outputs the invalid token error', function (done) {
-    const script = '12-payload-invalid.js';
+    const script = '12-syntax-invalid.js';
     cli()
       .run(`--space-id ${SOURCE_TEST_SPACE} ./examples/${script}`)
       .expect(assert.payload.syntaxError(script, 2, 'Invalid or unexpected token'))
