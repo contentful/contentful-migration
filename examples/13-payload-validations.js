368ec5d69095c125915579ef733926a91e57b3c9
Sergiy Tupchiy <sergiy@contentful.com>
2017-11-10 14:51:04 +0100

    test(e2e): payload validations - added comments to example

--- examples/13-payload-validations.js
+++ examples/13-payload-validations.js
@@ -1,5 +1,6 @@
 // Example of payload validation:
 module.exports = function (migration) {
+  // can't create content-type without name property
   migration.createContentType('berlin', {
     description: 'Where content is born'
   });
@@ -10,6 +11,7 @@ module.exports = function (migration) {
 
   const area = migration.editContentType('london');
 
+  // can't create field without name and type property
   area.createField('population');
 
   const barcelona = migration.createContentType('barcelona', {
@@ -22,8 +24,10 @@ module.exports = function (migration) {
 
   const paella = barcelona.editField('paella');
 
+  // can't set new ID for the field with requirements mismatch
   paella.newId('1uno');
 
+  // can't set a non existing field as a the displayField
   barcelona.displayField('area');
 
   const paris = migration.createContentType('paris', {
@@ -34,6 +38,7 @@ module.exports = function (migration) {
     .type('Symbol')
     .name('what it tastes like');
 
+  // can't create field with wrong(misspelled) type value
   paris.createField('frog')
     .type('Nmber')
     .name('what?');
@@ -42,14 +47,16 @@ module.exports = function (migration) {
 
   const baguette = paris.editField('baguette');
 
+  // can't set field to deleted state without ommitting beforehand
   baguette.deleted(true);
 
   paris.deleteField('baguette');
 
   const warsaw = migration.createContentType('warsaw', {
-    name: 'croissant'
+    name: 'Fasolka po bretonsku'
   });
 
+  // can't create field with duplicates in validations array
   warsaw.createField('pierogi')
     .type('Object')
     .name('najlepszy')
@@ -59,6 +66,7 @@ module.exports = function (migration) {
     name: 'zamek'
   });
 
+  // can't create field with invalid validations property
   krakow.createField('Żubrówka')
     .type('Number')
     .name('mocna')
@@ -68,6 +76,7 @@ module.exports = function (migration) {
     name: 'ein prosit ein prosit'
   });
 
+  // can't create field with invalid property type in validations
   munich.createField('Helles')
     .type('Location')
     .name('hendl')
