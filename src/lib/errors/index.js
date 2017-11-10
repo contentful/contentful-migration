8b704d14225b0a26bc96dfa84d23ea72c238aea3
Stephan Schneider <stephanschndr@gmail.com>
2017-11-10 09:05:15 +0100

    refactor(lib/errors): move to TypeScript

--- src/lib/errors/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  StepsValidationError: require('./steps-validation'),
-  ChunksValidationError: require('./chunks-validation'),
-  SpaceAccessError: require('./space-access'),
-  PayloadValidationError: require('./payload-validation')
-
-};
