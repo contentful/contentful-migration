9706658e9e4326c86cc68efb0d37c21196e98194
Tim Beyer <tim.beyer@gmail.com>
2017-11-16 15:33:52 +0100

    fix(transform-entries): allow newly created fields as from and to

--- test/unit/lib/migration-chunks/validation/transform-entries.spec.ts
+++ test/unit/lib/migration-chunks/validation/transform-entries.spec.ts
@@ -84,6 +84,27 @@ describe('Transform entries chunk validation', function () {
     })
   })
 
+  describe('when transforming entries to a just created field', function () {
+    it('returns no error', async function () {
+      const func = () => { console.log('transform') }
+      const contentTypes = [{
+        fields: [{ id: 'field', type: 'Symbol', description: 'A field'}],
+        sys: { id: 'dog' }
+      }]
+      const errors = await validateChunks(function up (migration) {
+        migration.editContentType('dog').createField('toField').type('Symbol')
+        migration.transformEntries({
+          contentType: 'dog',
+          from: ['field'],
+          to: ['toField'],
+          transformEntryForLocale: func
+        })
+      }, contentTypes)
+
+      expect(errors).to.eql([])
+    })
+  })
+
   describe('when transforming entries of a not yet created content type', function () {
     it('returns an error', async function () {
       const func = () => { console.log('transform') }
@@ -138,7 +159,7 @@ describe('Transform entries chunk validation', function () {
         migration.transformEntries({
           contentType: 'dog',
           from: ['surname'],
-          to: ['name'],
+          to: ['firstname'],
           transformEntryForLocale: func
         })
       }, contentTypes)
@@ -153,14 +174,14 @@ describe('Transform entries chunk validation', function () {
               contentTypeId: 'dog',
               transformation: {
                 from: ['surname'],
-                to: ['name'],
+                to: ['firstname'],
                 transformEntryForLocale: func
               }
             },
             type: 'contentType/transformEntries'
           }
         },
-        message: 'You cannot transform entries for content type "dog" because "surname", "name" can\'t be found on it.',
+        message: 'You cannot transform entries for content type "dog" because "surname", "firstname" can\'t be found on it.',
         type: 'InvalidEntriesTransformation'
       }])
     })
