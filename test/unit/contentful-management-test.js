4e8d40cb7dd13d83e48961dd55637f6c24eb3907
Benedikt Rötsch <benedikt@contentful.com>
2018-07-26 15:24:16 +0200

    test(unit): add test for custom user agent header

--- test/unit/contentful-management-test.js
+++ test/unit/contentful-management-test.js
@@ -27,18 +27,38 @@ test('Passes along HTTP client parameters', (t) => {
   t.end()
 })
 
-test('Generate the correct User Agent Header', (t) => {
+test('Generate the correct default User Agent Header', (t) => {
   createClientRewireApi.__Rewire__('axios', {create: sinon.stub()})
 
   const createHttpClientStub = sinon.stub()
   createClientRewireApi.__Rewire__('createHttpClient', createHttpClientStub)
   createClientRewireApi.__Rewire__('createContentfulApi', sinon.stub().returns({}))
-  createClient({accessToken: 'accesstoken', application: 'myApplication/1.1.1', integration: 'myIntegration/1.0.0'})
+  createClient({accessToken: 'accesstoken'})
   const headerParts = createHttpClientStub.args[0][1].headers['X-Contentful-User-Agent'].split('; ')
-  t.equal(headerParts.length, 5)
+  t.equal(headerParts.length, 3)
+  t.true(headerParts[0].match(/^sdk contentful-management\.js\/.+/))
+  t.true(headerParts[1].match(/^platform (.+\/.+|browser)/))
+  t.true(headerParts[2].match(/^os .+/))
+
+  createClientRewireApi.__ResetDependency__('createHttpClient')
+  createClientRewireApi.__ResetDependency__('wrapHttpClient')
+  createClientRewireApi.__ResetDependency__('createContentfulApi')
+  t.end()
+})
+
+test('Generate the correct custom User Agent Header', (t) => {
+  createClientRewireApi.__Rewire__('axios', {create: sinon.stub()})
+
+  const createHttpClientStub = sinon.stub()
+  createClientRewireApi.__Rewire__('createHttpClient', createHttpClientStub)
+  createClientRewireApi.__Rewire__('createContentfulApi', sinon.stub().returns({}))
+  createClient({accessToken: 'accesstoken', application: 'myApplication/1.1.1', integration: 'myIntegration/1.0.0', feature: 'some-feature'})
+  const headerParts = createHttpClientStub.args[0][1].headers['X-Contentful-User-Agent'].split('; ')
+  t.equal(headerParts.length, 6)
   t.equal(headerParts[0], 'app myApplication/1.1.1')
   t.equal(headerParts[1], 'integration myIntegration/1.0.0')
-  t.equal(headerParts[2], `sdk contentful-management.js/${version}`)
+  t.equal(headerParts[2], 'feature some-feature')
+  t.equal(headerParts[3], `sdk contentful-management.js/${version}`)
 
   createClientRewireApi.__ResetDependency__('createHttpClient')
   createClientRewireApi.__ResetDependency__('wrapHttpClient')
