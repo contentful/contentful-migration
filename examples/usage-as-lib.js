f77d47c6d798ae2ff4e8ce413303461a38cd640c
Charles-P. Clermont <charles@cpclermont.com>
2018-07-31 15:10:53 -0400

    Add usage as library e2e test

---
+++ examples/usage-as-lib.js
@@ -0,0 +1,24 @@
+const { runMigration } = require('../built/bin/cli');
+
+async function main (filePath) {
+  let statusCode = 0;
+
+  try {
+    await runMigration({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
+      environmentId: 'master',
+      yes: true,
+      filePath
+    });
+  } catch (e) {
+    statusCode = 1;
+    console.log('Catching Error');
+  } finally {
+    console.log('Cleaning Up');
+  }
+
+  process.exit(statusCode);
+}
+
+main(process.argv[2]);
