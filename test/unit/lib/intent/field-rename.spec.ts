b571691da7e32400806f6a2fd2a49fcf1d8f4817
Khaled Garbaya <khaled@contentful.com>
2018-09-27 17:14:52 +0200

    test(field-rename): Add tests

--- test/unit/lib/intent/field-rename.spec.ts
+++ test/unit/lib/intent/field-rename.spec.ts
@@ -5,11 +5,14 @@ import runIntent from './run-intent'
 import fakeCallsite from '../../../helpers/fake-callsite'
 import makeApiEntry from '../../../helpers/make-api-entry'
 
+import { EditorInterfaces } from '../../../../src/lib/entities/content-type'
+
 describe('FieldRenameIntent', function () {
+
   describe('when renaming a field of a content type', function () {
+
     it('renames the field on all entries', async function () {
       const intent: FieldRenameIntent = actionCreators.field.rename('dog', 0, 'bits', 0, fakeCallsite(), 'bites')
-
       const contentTypes = [{
         name: 'Doggy',
         sys: {
@@ -85,6 +88,61 @@ describe('FieldRenameIntent', function () {
           }
         })
       ])
+    })
+  })
+
+  describe('rename Fields with editor interfaces', function () {
+
+    it('keeps the editor interface when renaming a field', async function () {
+
+      const editorInterfacesByContentType:Map<String, EditorInterfaces> = new Map()
+      editorInterfacesByContentType.set('dog', {controls: [{ widgetId: 'dropdown', fieldId: 'bits'}]})
+      const entries = [
+        makeApiEntry({
+          id: 'bello',
+          contentTypeId: 'dog',
+          version: 1,
+          fields: {
+            bits: {
+              'en-US': 'a lot',
+              'de-DE': 'ganz schön viele'
+            },
+            goodboys: { 'en-US': 0 }
+          }
+        }),
+        makeApiEntry({
+          id: 'waldo',
+          contentTypeId: 'dog',
+          version: 2,
+          fields: {
+            bits: {
+              'en-US': 'just a few',
+              'de-DE': 'nur n paar'
+            },
+            goodboys: { 'en-US': 9001 }
+          }
+        })
+      ]
+      const intent: FieldRenameIntent = actionCreators.field.rename('dog', 0, 'bits', 0, fakeCallsite(), 'bites')
+      const contentTypes = [{
+        name: 'Doggy',
+        sys: {
+          id: 'dog',
+          version: 1
+        },
+        fields: [
+          {
+            id: 'bits',
+            type: 'Symbol'
+          }, {
+            id: 'goodboys',
+            type: 'Number'
+          }
+        ]
+      }]
+
+      const api = await runIntent(intent, contentTypes, entries, [], editorInterfacesByContentType)
+      expect(true).to.eq(false)
     })
   })
 })
