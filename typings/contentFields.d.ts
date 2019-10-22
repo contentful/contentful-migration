213c9525bff4674f66684e927315f2b0956aff41
Johann <johann.roehl@contentful.com>
2019-10-22 14:29:51 +0200

    fix(ts): Add missing ContentFields properties (#239)

--- typings/contentFields.d.ts
+++ typings/contentFields.d.ts
@@ -1,8 +1,49 @@
-export interface ContentFields {
-  id: string,
-  name: string,
-  required: boolean,
-  localized: boolean,
-  type: string,
+export interface NumRange {
+  min?: number
+  max?: number
+}
 
+export interface DateRange {
+  min?: string
+  max?: string
+}
+
+export interface RegExp {
+  pattern: string
+  flags: string
+}
+
+export interface Validation {
+  linkContentType?: string[]
+  in?: string[]
+  linkMimetypeGroup?: string[]
+  enabledNodeTypes?: string[]
+  enabledMarks?: string[]
+  unique?: boolean
+  size?: NumRange
+  range?: NumRange
+  dateRange?: DateRange
+  regexp?: RegExp
+  prohibitRegexp?: RegExp
+  assetImageDimensions?: {
+    width?: NumRange
+    height?: NumRange
+  }
+  assetFileSize?: NumRange
+}
+
+export interface Item {
+  type: string
+  linkType?: string
+  validations?: Validation[]
+}
+
+export interface ContentFields extends Item {
+  id: string
+  name: string
+  required: boolean
+  localized: boolean
+  disabled?: boolean
+  omitted?: boolean
+  items?: Item
 }
