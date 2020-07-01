5be58bac4325850cbcd25cfc2841a8d0f77285b6
Janko Marklein <janko.marklein@contentful.com>
2020-07-01 13:41:46 +0200

    feat(tags): add tag update action

--- src/lib/action/tag-update.ts
+++ src/lib/action/tag-update.ts
@@ -1,17 +1,28 @@
-import { APIAction } from './action'
-import OfflineAPI from '../offline-api/index'
+import { EntityAction } from './action'
+import Tag from '../entities/tag'
 
-class TagUpdateAction extends APIAction {
+class TagUpdateAction extends EntityAction {
+  private props: object
   private tagId: string
 
-  constructor (tagId: string) {
+  constructor (tagId: string, props: object) {
     super()
+    this.props = props
     this.tagId = tagId
   }
 
-  async applyTo (api: OfflineAPI) {
-    await api.updateTag(this.tagId)
+  getEntityId (): string {
+    return this.tagId
   }
+
+  getEntityType (): string {
+    return 'TAG'
+  }
+
+  async applyTo (tag: Tag) {
+    Object.assign(tag, this.props)
+  }
+
 }
 
 export { TagUpdateAction }
