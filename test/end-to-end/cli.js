eba68185f1176655b487cc4f266b909879fc3a0c
Janko Marklein <janko.marklein@contentful.com>
2020-07-03 16:30:51 +0200

    fix(test): fake commit

--- test/end-to-end/cli.js
+++ test/end-to-end/cli.js
@@ -2,7 +2,10 @@
 
 const nixt = require('nixt');
 
+// TODO: Why don't these work?
+
 const ACCESS_TOKEN = process.env.CONTENTFUL_INTEGRATION_MANAGEMENT_TOKEN;
+console.log('ACCESS_TOKEN: ', ACCESS_TOKEN);
 process.env.TEST_ENVIRONMENT = 'end-to-end-tests';
 const cli = () => {
   return nixt()
