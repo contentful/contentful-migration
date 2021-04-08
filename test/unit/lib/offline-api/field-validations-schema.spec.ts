6be4481575df22ae616081003f8d73a6445a43b9
Yiotis Kaltsikis <giotisgr@gmail.com>
2021-04-08 16:34:18 +0200

    test: dynamic creation of field validations schema

---
+++ test/unit/lib/offline-api/field-validations-schema.spec.ts
@@ -0,0 +1,26 @@
+'use strict'
+
+import { expect } from 'chai'
+import * as Joi from 'joi'
+import sinon from 'sinon'
+import * as configModule from '../../../../src/bin/lib/config';
+import getFieldValidations from '../../../../src/lib/offline-api/validator/schema/field-validations-schema'
+
+describe.only('makeFieldValidations', function () {
+  const hasAlphaHeaderStub = sinon.stub(configModule, "hasAlphaHeader")
+  describe('with the assembly-types alpha header defined', () => {
+    it('should include the relationshipType validation', () => {
+      hasAlphaHeaderStub.returns(true)
+      const schema = getFieldValidations()
+      expect(Joi.validate({ relationshipType: ['Composition'] }, schema).error).eq(null)
+      expect(Joi.validate({ relationshipType: ['invalid'] }, schema).error).not.to.eq(null)
+    })
+  })
+  describe('without the assembly-types alpha header defined', () => {
+    it('should not include the relationshipType validation', () => {
+      hasAlphaHeaderStub.returns(false)
+      const schema = getFieldValidations()
+      expect(Joi.validate({ relationshipType: ['Composition'] }, schema).error).not.to.eq(null)
+    })
+  })
+})
