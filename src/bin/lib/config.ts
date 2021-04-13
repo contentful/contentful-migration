6d1c211582d10bb3adc8ae144e935342f78cc9d6
Yiotis Kaltsikis <giotisgr@gmail.com>
2021-04-13 16:20:00 +0200

    chore: initialize global conf to empty obj

--- src/bin/lib/config.ts
+++ src/bin/lib/config.ts
@@ -10,7 +10,7 @@ const configPath = path.resolve(homedir, '.contentfulrc.json')
 // enable/disable certain behaviors:
 // e.g. the relationshipType field validation can be used only when
 // the assembly-types alpha header is present in the config.
-let globalConfig: ClientConfig
+let globalConfig: ClientConfig = { headers: {} }
 
 interface ClientConfig {
   accessToken?: string
@@ -110,11 +110,8 @@ function getConfig (argv) {
 }
 
 const hasAlphaHeader = (feature: 'assembly-types') => {
-  if (globalConfig) {
-    const alphaHeader = globalConfig.headers['x-contentful-enable-alpha-feature'] as string || ''
-    return alphaHeader.includes(feature)
-  }
-  return false
+  const alphaHeader = globalConfig.headers['x-contentful-enable-alpha-feature'] as string || ''
+  return alphaHeader.includes(feature)
 }
 
 export default getConfig
