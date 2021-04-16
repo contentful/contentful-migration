11897925f22e37d0d887a2645c95116d55a2267e
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2021-04-16 12:36:44 +0200

    fix(tags): fetch tags in entry-transform

--- test/unit/lib/fetcher.spec.ts
+++ test/unit/lib/fetcher.spec.ts
@@ -495,55 +495,139 @@ describe('Fetcher', function () {
     expect(localeCodes).to.eql(result)
   })
 
-  it('fetches all tags in the enviroment', async function () {
-    const intents = await buildIntents(function up (migration) {
-      migration.createTag('person', {
-        name: 'bar',
-        description: 'A content type for a person'
+  it("fetches all tags in the enviroment", async function () {
+    const intents = await buildIntents(
+      function up(migration) {
+        migration.createTag("person", {
+          name: "bar",
+          description: "A content type for a person",
+        });
+      },
+      noOp,
+      {}
+    );
+
+    const request = sinon.stub();
+    request
+      .withArgs({
+        method: "GET",
+        url: `/tags?limit=100&order=sys.createdAt&skip=0`,
       })
-    }, noOp, {})
+      .resolves({
+        items: [
+          {
+            name: "Person Tag",
+            sys: { id: "person", type: "Tag", visbility: 'private' },
+          },
+          {
+            name: "A very goodboy",
+            sys: { id: "dog", type: "Tag", visbility: 'public' },
+          },
+        ],
+        total: 2,
+        limit: 2,
+      });
+
+    const intentList = new IntentList(intents);
+
+    const fetcher = new Fetcher(request);
+    const tags = await fetcher.getTagsForEnvironment(intentList);
+
+    expect(request).to.have.been.calledWith({
+      method: "GET",
+      url: `/tags?limit=100&order=sys.createdAt&skip=0`,
+    });
+
+    expect(tags).to.eql([
+      {
+        name: "Person Tag",
+        sys: { id: "person", type: "Tag", visbility: 'private' },
+      },
+      {
+        name: "A very goodboy",
+        sys: { id: "dog", type: "Tag", visbility: 'public' },
+      },
+    ]);
+  });
+
+  it('fetches tags in the environment when the intent is \'entry-transform\'', async function () {
+    const intents = await buildIntents(function up (migration) {
+      migration.transformEntries({
+        contentType: 'newsArticle',
+        from: ['author', 'authorCity'],
+        to: ['byline'],
+        transformEntryForLocale: function (fromFields, currentLocale) {
+          if (currentLocale === 'de-DE') {
+            return
+          }
+          const newByline = `${fromFields.author[currentLocale]} ${fromFields.authorCity[currentLocale]}`
+          return { byline: newByline }
+        }
+      })
+    }, null, null)
 
     const request = sinon.stub()
     request
       .withArgs({
         method: 'GET',
-        url: `/tags?limit=100&order=sys.createdAt&skip=0`
+        url: `/entries?limit=100&order=sys.createdAt&sys.archivedAt[exists]=false&sys.contentType.sys.id[in]=newsArticle&skip=0`
+      })
+      .resolves({
+        skip: 0,
+        limit: 4,
+        total: 6,
+        items: ['item1', 'item2', 'item3', 'item4']
+      })
+    request
+      .withArgs({
+        method: 'GET',
+        url: `/entries?limit=100&order=sys.createdAt&sys.archivedAt[exists]=false&sys.contentType.sys.id[in]=newsArticle&skip=4`
+      })
+      .resolves({
+        skip: 4,
+        limit: 4,
+        total: 6,
+        items: ['item5', 'item6']
+      })
+      request
+      .withArgs({
+        method: "GET",
+        url: `/tags?limit=100&order=sys.createdAt&skip=0`,
       })
       .resolves({
         items: [
           {
-            name: 'Person Tag',
-            sys: { 'id': 'person', 'type': 'Tag' }
+            name: "Person Tag",
+            sys: { id: "person", type: "Tag", visibility: 'private' },
           },
           {
-            name: 'A very goodboy',
-            sys: { 'id': 'dog', 'type': 'Tag' }
-          }
+            name: "A very goodboy",
+            sys: { id: "dog", type: "Tag", visibility: 'public' },
+          },
         ],
         total: 2,
-        limit: 2
-      })
+        limit: 2,
+      });
 
     const intentList = new IntentList(intents)
 
     const fetcher = new Fetcher(request)
-    const tags = await fetcher.getTagsForEnvironment(intentList)
+    const entries = await fetcher.getEntriesInIntents(intentList)
+    const tags = await fetcher.getTagsForEnvironment(intentList);
 
-    expect(request).to.have.been.calledWith({
-      method: 'GET',
-      url: `/tags?limit=100&order=sys.createdAt&skip=0`
-    })
 
+    const result = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6']
+    expect(entries).to.eql(result)
     expect(tags).to.eql([
       {
-        name: 'Person Tag',
-        sys: { 'id': 'person', 'type': 'Tag' }
+        name: "Person Tag",
+        sys: { id: "person", type: "Tag", visibility: 'private' },
       },
       {
-        name: 'A very goodboy',
-        sys: { 'id': 'dog', 'type': 'Tag' }
-      }
-    ])
+        name: "A very goodboy",
+        sys: { id: "dog", type: "Tag", visibility: 'public' },
+      },
+    ]);
   })
 
   it('fetches with a given limit', async function () {
