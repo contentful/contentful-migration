f494e0e0d38f3ff6feb547aeb3c7bb78d1fd17ff
Rebecca Koenig <rebecca.koenig@contentful.com>
2021-12-10 09:04:31 +0100

    chore: remove unused types

--- src/lib/action/editor-layout/editor-layout-create-field-group.ts
+++ src/lib/action/editor-layout/editor-layout-create-field-group.ts
@@ -1,20 +1,6 @@
 import { EditorInterfaces } from '../../entities/content-type'
 import { EntityAction, EntityType } from '../action'
 
-interface FieldGroupItem {
-  groupId: string
-  name: string
-  items: EditorLayoutItem[]
-}
-
-interface FieldItem {
-  fieldId: string
-}
-
-type EditorLayoutItem = FieldItem | FieldGroupItem
-
-export type EditorLayout = EditorLayoutItem[]
-
 class EditorLayoutCreateFieldGroupAction extends EntityAction {
   private readonly contentTypeId: string
   private readonly fieldGroupId: string
