3d3f79ce19e54893ea9af84c955ac5aec256b6a4
Rebecca König <veu@users.noreply.github.com>
2022-04-06 11:07:10 +0200

    chore: number the editor layout test cases (#1050)

--- examples/xx-change-field-group-id-editor-layout.js
+++
@@ -1,7 +0,0 @@
-// TODO: rename file before merging to master
-
-module.exports = function (migration) {
-  const page = migration.editContentType('page');
-  const editorLayout = page.editEditorLayout();
-  editorLayout.changeFieldGroupId('metadata', 'info');
-};
