e038dcafe155ab4772a0164a0f6df4be1ce1149f
Bas Kiers <kiers.bas@gmail.com>
2023-06-20 14:38:42 +0200

    feat: add RichText fields to allowedResources validation

--- src/lib/utils/resource-links.ts
+++ src/lib/utils/resource-links.ts
@@ -1,5 +1,19 @@
 export const MAX_ALLOWED_RESOURCES = 3
 export const MAX_RESOURCE_LINKS = 3
 
+export const RICH_TEXT_RESOURCE_LINK_NODES = ['embedded-resource-block']
+
 export const isResourceLink = (field) =>
   field.type === 'ResourceLink' || (field.type === 'Array' && field.items?.type === 'ResourceLink')
+
+export const getEnabledResourceLinkNodes = (field): string[] | undefined => {
+  if (field.type !== 'RichText' || !Array.isArray(field.validations)) {
+    return undefined
+  }
+
+  for (const { enabledNodeTypes } of field.validations) {
+    if (Array.isArray(enabledNodeTypes)) {
+      return RICH_TEXT_RESOURCE_LINK_NODES.filter((nodeType) => enabledNodeTypes.includes(nodeType))
+    }
+  }
+}
