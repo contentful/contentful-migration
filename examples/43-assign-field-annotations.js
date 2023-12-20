15462dd75c5bb7edaa938b9605874438942d7c06
Kado <kadodamball@hotmail.com>
2023-12-20 10:26:13 +0100

    feat: support graphQL annotations

--- examples/43-assign-field-annotations.js
+++ examples/43-assign-field-annotations.js
@@ -1,4 +1,17 @@
 module.exports = function (migration) {
   const annotatedContentType = migration.editContentType('annotated')
   annotatedContentType.editField('sources').setAnnotations(['Contentful:AggregateComponent'])
+  annotatedContentType.editField('title').setAnnotations([
+    {
+      sys: {
+        type: 'Link',
+        linkType: 'Annotation',
+        id: 'Contentful:GraphQLFieldResolver'
+      },
+      parameters: {
+        appFunctionId: '123',
+        appDefinitionId: '456'
+      }
+    }
+  ])
 }
