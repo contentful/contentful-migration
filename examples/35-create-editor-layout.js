286605efaac916a99a44030efaaeb0f87b82109f
Ely Lucas <ely@meta-tek.net>
2025-06-02 13:26:27 -0600

    chore: add comments to editor layout example [ZEND-6400] (#1498)
    * Create 56-create-first-field-group.js
    
     * Starting with a new content type with 2 fields
     * Content type has no existing field groups
     * Goal is to create a field group and add 2 fields to that group
    Note that a new editor tab should be created for the changes to make effect. Otherwise, no changes will be applied.
    
    * chore: add comments to editor layout example
    
    * chore: remove the added move sample as it was failing the tests
    
    * chore: added missing line to fix tests
    
    ---------
    
    Co-authored-by: Alex Zi <VikingRock@users.noreply.github.com>

--- examples/35-create-editor-layout.js
+++ examples/35-create-editor-layout.js
@@ -1,3 +1,6 @@
+/**
+ * Demonstrates how to create an editor layout with field groups and tabs.
+ */
 module.exports = function (migration) {
   const page = migration.createContentType('page').name('Page')
   page.createField('name').name('Internal name').type('Symbol')
@@ -6,18 +9,21 @@ module.exports = function (migration) {
   // an editor layout can be created empty but not saved without adding at least one tab
   const editorLayout = page.createEditorLayout()
 
+  // create the field group that will become the top level tab
   editorLayout.createFieldGroup('content', {
     name: 'Content'
   })
 
+  // make the Content field group a top level tab
   editorLayout.changeFieldGroupControl('content', 'builtin', 'topLevelTab', {
     helpText: 'Main content'
   })
 
+  // create field groups inside the content tab
   editorLayout.createFieldGroup('settings').name('Settings')
-
   editorLayout.editFieldGroup('settings').createFieldGroup('seo').name('SEO')
 
+  // change the field group control to a fieldset (this is what makes it a section)
   editorLayout.changeFieldGroupControl('seo', 'builtin', 'fieldset', {
     helpText: 'Search related fields',
     collapsedByDefault: false
