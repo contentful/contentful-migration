2202ec5e197525ea7c0d5b4552d7b01d76c31723
Pierre Marais <pierre.marais@contentful.com>
2025-07-04 14:49:49 +0200

    chore: revert chai-as-promised change to do in separate pr

--- test/setup.js
+++ test/setup.js
@@ -3,8 +3,10 @@
 const chai = require('chai')
 const dirtyChai = require('dirty-chai')
 const sinonChai = require('sinon-chai')
-const chaiAsPromised = require('chai-as-promised')
+const chaiAsPromised = import('chai-as-promised')
 
-chai.use(sinonChai)
-chai.use(dirtyChai)
-chai.use(chaiAsPromised)
+;(async () => {
+  chai.use(sinonChai)
+  chai.use(dirtyChai)
+  chai.use(await chaiAsPromised)
+})()
