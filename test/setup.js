269657de70caaec8c8ae8dce44800362fe16c45f
Pierre Marais <pierre.marais@contentful.com>
2025-07-04 14:51:12 +0200

    chore: remove unused library

--- test/setup.js
+++ test/setup.js
@@ -3,10 +3,6 @@
 const chai = require('chai')
 const dirtyChai = require('dirty-chai')
 const sinonChai = require('sinon-chai')
-const chaiAsPromised = import('chai-as-promised')
 
-;(async () => {
-  chai.use(sinonChai)
-  chai.use(dirtyChai)
-  chai.use(await chaiAsPromised)
-})()
+chai.use(sinonChai)
+chai.use(dirtyChai)
